3097daaa93768e3a30c83b8654ce40fd85b10ef7
qwex03 <qlecordier@gmail.com>
2024-11-05 11:57:13 +0100

    pages statiques

--- src/Controller/GestionController.php
+++ src/Controller/GestionController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
+use App\Repository\EtatRepository;
+use App\Repository\HistoriqueRepository;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +13,31 @@ use Symfony\Component\Routing\Attribute\Route;
 class GestionController extends AbstractController
 {
     #[Route('/gestion', name: 'app_gestion')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository, EtatRepository $etatRepository, HistoriqueRepository $historiqueRepository, CategorieRepository $categorieRepository): Response
     {
+        $nbProduit = $produitRepository->count();
+
+        $etatCasse = $etatRepository->findOneBy(["nom" => "hors d'usage"]);
+        $nbProduitsCasses = $produitRepository->count(['id_etat' => $etatCasse]);
+
+        $etatReparer = $etatRepository->findOneBy(["nom" => "à réparer"]);
+        $nbProduitsRepar = $produitRepository->count(['id_etat' => $etatReparer]);
+
+        $enPret = $historiqueRepository->count(["retour"=>0]);
+        $retourné = $historiqueRepository->count(["retour"=>1]);
+
+        $nbcat = $categorieRepository->count();
+
         return $this->render('gestion/index.html.twig', [
-            'controller_name' => 'GestionController',
+            'nbproduit' => $nbProduit,
+            'nbproduithorsdusage' => $nbProduitsCasses,
+            'nbproduitARepar' => $nbProduitsRepar,
+            'nbPret' => $enPret,
+            'nbRetour' => $retourné,
+            'nbCat' => $nbcat,
+            'nbRetour' => '',
         ]);
     }
 
-    
+
 }
